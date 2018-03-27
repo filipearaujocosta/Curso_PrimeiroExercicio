0139a33971543fcb9910ccc235fa92aa61899a87
Filipe Costa <filipe.tcn@gmail.com>
2018-03-27 17:33:45 -0300

    initial Commit

---
+++ Curso_PrimeiroExercicio/Pedido.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_PrimeiroExercicio
+{
+    class Pedido
+    {
+        public string Codigo { get; set;}
+        public string Dia { get; set; }
+        public string Mes { get; set; }
+        public string Ano { get; set; }
+        public string QtItem { get; set; }
+        public string CodProd { get; set; }
+        public string Porc { get; set; }
+
+        public void fazerPedido()
+        {
+            List<Pedido> ListPedido = new List<Pedido>();
+            Console.WriteLine("Digite os dados do Pedido:");
+            Console.WriteLine("Código:");
+            var codigo = Console.ReadLine();
+
+
+
+            Console.WriteLine("Quantos itens tem o Pedido?");
+            var ItQtd = Console.ReadLine();
+
+            for (int x = 1; x <= int.Parse(ItQtd); x++)
+            {
+
+                Pedido ped = new Pedido();
+                ped.Codigo = codigo;
+                Console.WriteLine("Código:");
+                ped.Codigo = Console.ReadLine();
+                //Console.WriteLine("Descricao:");
+                //ped.Descricao = Console.ReadLine();
+                //Console.WriteLine("Preco:");
+                //ped.Preco = Console.ReadLine();
+
+                ListPedido.Add(ped);
+            }
+
+        }
+    }
+}
---
+++ Curso_PrimeiroExercicio/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_PrimeiroExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> ProdList = new List<Produto>();
+           
+            for (int x = 0; x <=3; x++)
+            {
+                Produto prod = new Produto();
+                Console.WriteLine("Digite os dados do Produto:");
+                Console.WriteLine("Código:");
+                prod.Codigo = Console.ReadLine();
+                Console.WriteLine("Descricao:");
+                prod.Descricao = Console.ReadLine();
+                Console.WriteLine("Preco:");
+                prod.Preco = Console.ReadLine();
+
+                ProdList.Add(prod);
+            }
+
+            foreach(Produto p in ProdList)
+            {
+                Console.WriteLine($"Codigo: {p.Codigo}, Descricao: {p.Descricao},Preco: {p.Preco}");
+            }
+
+           
+
+            foreach (Produto p in ProdList)
+            {
+                Console.WriteLine($"Codigo: {p.Codigo}, Descricao: {p.Descricao},Preco: {p.Preco}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

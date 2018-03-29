d5a7bb76d6a1421dd93e2883f7ba848d138ec632
Matheus Antunes <matantunes97@gmail.com>
2018-03-29 07:37:34 -0300

    Adicionando cadastro de pedido e exibindo

--- Curso_PrimeiroExercicio/Produto.cs
+++ Curso_PrimeiroExercicio/Produto.cs
@@ -11,7 +11,16 @@ namespace Curso_PrimeiroExercicio
         public string Codigo { get; set;}
         public string Descricao { get; set; }
         public string Preco { get; set; }
-
-       
+        
     }
+    class Pedido
+    {
+        public string codigo { get; set; }
+        public string qta { get; set; }
+        public string Porcent { get; set; }
+
+
+
+    }
+
 }
--- Curso_PrimeiroExercicio/Program.cs
+++ Curso_PrimeiroExercicio/Program.cs
@@ -11,35 +11,95 @@ namespace Curso_PrimeiroExercicio
     {
         static void Main(string[] args)
         {
+
+            // Cadastramento de produtos
             List<Produto> ProdList = new List<Produto>();
            
-            for (int x = 0; x <=3; x++)
+            for (int x = 1; x <=2; x++)
             {
                 Produto prod = new Produto();
                 Console.WriteLine("Digite os dados do Produto:");
-                Console.WriteLine("Código:");
+                Console.Write("Código: ");
                 prod.Codigo = Console.ReadLine();
-                Console.WriteLine("Descricao:");
+                Console.Write("Descricao:");
                 prod.Descricao = Console.ReadLine();
-                Console.WriteLine("Preco:");
+                Console.Write("Preco:");
                 prod.Preco = Console.ReadLine();
 
                 ProdList.Add(prod);
             }
 
-            foreach(Produto p in ProdList)
-            {
-                Console.WriteLine($"Codigo: {p.Codigo}, Descricao: {p.Descricao},Preco: {p.Preco}");
-            }
+            Console.WriteLine();
 
-           
-
+            // listando os PRODUTOS CADASTRADOS
             foreach (Produto p in ProdList)
             {
-                Console.WriteLine($"Codigo: {p.Codigo}, Descricao: {p.Descricao},Preco: {p.Preco}");
+                Console.WriteLine($"Codigo : {p.Codigo}  ,Descricao: {p.Descricao}  ,Preco: {p.Preco}");
+            }
+        
+            //Cadastramento de um pedido
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite os dados do pedido");
+
+            Console.Write("Código do pedido: ");
+            int Codigo= Convert.ToInt16( Console.ReadLine());
+
+            Console.Write("Dia: ");
+            var dia = Console.ReadLine();
+
+            Console.Write("mês: ");
+            var mes = Console.ReadLine();
+
+            Console.Write("ano: ");
+            var ano = Console.ReadLine();
+
+            Console.Write("Quantos itens tem o pedido: ");
+            int qtItem = Convert.ToInt16(Console.ReadLine());
+
+            List<Pedido> Cadastramentopedido = new List<Pedido>();
+            for (int x = 1; x <= qtItem; x++)
+            {
+                Pedido pedido = new Pedido();
+                Console.Write("Digite os dados do" + x +"item");
+                Console.Write("Código:");
+                pedido.codigo = Console.ReadLine();
+                Console.Write("Quantidade:");
+                pedido.qta = Console.ReadLine();
+                Console.Write("Porcentagem de desconto: ");
+                pedido.Porcent = Console.ReadLine();
+
+                Cadastramentopedido.Add(pedido);
             }
 
-            Console.ReadLine();
+
+            Console.WriteLine();
+
+            // Exibição dos dados de um pedido
+
+            Console.WriteLine("Digite o código do pedido:");
+            int numeroPedido =Console.Read();
+
+
+            if (numeroPedido == Codigo)
+            {
+                
+                    Console.WriteLine("Pedido : " + Codigo+ "    Data:"+ dia+"/" +mes+"/"+ano);
+                    Console.WriteLine("Itens:");
+
+                foreach (Pedido cadPedido in Cadastramentopedido)
+                {
+                    foreach (Produto p in ProdList)
+                    {
+                        Console.WriteLine(p.Descricao + "Preço: " + p.Preco + "Quantidade: " + cadPedido.qta+ "Desconto: "+ cadPedido.Porcent );
+                    }
+                    Console.ReadLine();
+                }
+            }
+
+
+            
 
         }
     }
